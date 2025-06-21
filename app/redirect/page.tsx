0e1457babb0b10170d07d968d2b541847aef3a81
fisofesefaso <122107290+fisofesefaso@users.noreply.github.com>
2025-06-21 17:03:16 +0800

    Add files via upload

--- app/redirect/page.tsx
+++ app/redirect/page.tsx
@@ -1,10 +1,6 @@
 import { redirect } from "next/navigation"
 
-export const metadata = {
-  robots: "noindex, noimageindex, noarchive, nosnippet"
-}
-
 export default async function RedirectPage() {
   // Replace with your actual biolink URL
-  redirect("https://server-roblox.com/games/126884695634066/Grow-a-Garden?privateServerLinkCode=60996196158654546780483711157260")
+  redirect("https://your-biolink-url.com")
 }
