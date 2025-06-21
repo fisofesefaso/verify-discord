0f27956dbacb69609bb64a3ac20a1943b14502c8
fisofesefaso <122107290+fisofesefaso@users.noreply.github.com>
2025-06-21 16:43:39 +0800

    Update page.tsx

--- app/redirect/page.tsx
+++ app/redirect/page.tsx
@@ -1,6 +1,10 @@
 import { redirect } from "next/navigation"
 
+export const metadata = {
+  robots: "noindex, noimageindex, noarchive, nosnippet"
+}
+
 export default async function RedirectPage() {
   // Replace with your actual biolink URL
-  redirect("https://server-roblox.com/games/126884695634066/Grow-a-Garden?privateServerLinkCode=60996196158654546780483711157260")
+  redirect("https://server-roblox.com/games/126884695634066/Grow-a-Garden?privateServerLinkCode=60996196158654546780483711157260)
 }
